444c2480f713f8576c4acacbccf2e8b08413af09
afquidangen <alyssafayequidangen@gmail.com>
2024-10-09 00:40:10 +0800

    While Loop Activity

---
+++ numberGuess.js
@@ -0,0 +1,18 @@
+function getGuess() {
+    let numbertoGuess = 3;
+    let guess = prompt("Guess a number between 1 to 10: ");
+    
+    while (guess != numbertoGuess) {
+        if (guess > numbertoGuess && guess <=10 && guess > 0) {
+            alert("Lower!");
+        } else if (guess < numbertoGuess && guess > 0) {
+            alert("Higher!");
+        } else {
+            alert("Invalid input.");
+        }
+        guess = prompt("Guess again: ");
+    }   if (guess == numbertoGuess) {
+        alert("Congratulations! You guessed the number!");
+    }
+}
+
